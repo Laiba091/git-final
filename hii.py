08f9aa3a6af598db916e019c8c608eec3a0994c2
Laiba091 <laibahumayun01@gmail.com>
2026-04-07 15:13:53 +0500

    initail files

---
+++ hii.py
@@ -0,0 +1,10 @@
+# Check if a number is even or odd
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+    print("its second file")
+    
